f5f1641395dbc3b844fde85651345d24dce402fd
Guy Marshall <guymarshall9@gmail.com>
2022-12-05 21:44:22 +0000

    Add forbid for unsafe_code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+#![forbid(unsafe_code)]
+
 /**
  * prompt user for password to check
  * read password.txt into memory
  * loop through passwords in password.txt
  * if user_password exists, print warning
  */
+
 
 mod user_input;
 
