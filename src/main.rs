ce15081ad5ded021acc097e8a7cdebf57c389c04
Guy Marshall <guymarshall9@gmail.com>
2022-12-05 21:22:14 +0000

    Hardcode password.txt instead of asking for filename

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,9 @@ use std::fs::File;
 use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
-    let password_filename: String = user_input::get_user_input("Enter filename:");
     let user_password: String = user_input::get_user_input("Enter password:");
 
-    let file: File = File::open(password_filename)?;
+    let file: File = File::open("passwords.txt")?;
     let reader: BufReader<File> = BufReader::new(file);
 
     for line in reader.lines() {
