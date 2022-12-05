16995db02eb6b9c44aeab2e4a232f6dbd6a1b743
Guy Marshall <guymarshall9@gmail.com>
2022-12-05 20:15:32 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+/**
+ * prompt user for password to check
+ * read password.txt into memory
+ * loop through passwords in password.txt
+ * if userPassword exists, print warning
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
