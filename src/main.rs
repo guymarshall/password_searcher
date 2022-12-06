6a5c677320309fa404627bce34a45f92e636ac4d
Guy Marshall <75757034+guymarshall@users.noreply.github.com>
2022-12-06 12:17:55 +0000

    Change hardcoded filename to filename variable

--- src/main.rs
+++ src/main.rs
@@ -15,8 +15,9 @@ use std::io::{self, prelude::*, BufReader};
 
 fn main() -> io::Result<()> {
     let user_password: String = user_input::get_user_input("Enter password:");
+    let filename: String = user_input::get_user_input("Enter filename:");
 
-    let file: File = File::open("passwords.txt")?;
+    let file: File = File::open(filename)?;
     let reader: BufReader<File> = BufReader::new(file);
 
     for line in reader.lines() {
